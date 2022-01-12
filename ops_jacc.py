3196b704fe95479dde7cbe1c16d4a6dfa9ee0cd9
Ashley Bruce <60762330+ashleyybrucee@users.noreply.github.com>
2022-01-12 11:05:57 -0800

    Add files via upload

---
+++ ops_jacc.py
@@ -0,0 +1,188 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class GraphUnet(nn.Module):
+
+    def __init__(self, ks, in_dim, out_dim, dim, act, drop_p):
+        super(GraphUnet, self).__init__()
+        self.ks = ks
+        self.bottom_gcn = GCN(dim, dim, act, drop_p)
+        self.down_gcns = nn.ModuleList()
+        self.up_gcns = nn.ModuleList()
+        self.pools = nn.ModuleList()
+        self.unpools = nn.ModuleList()
+        self.l_n = len(ks)
+        for i in range(self.l_n):
+            self.down_gcns.append(GCN(dim, dim, act, drop_p))
+            self.up_gcns.append(GCN(dim, dim, act, drop_p))
+            self.pools.append(Pool(ks[i], dim, drop_p))
+            self.unpools.append(Unpool(dim, dim, drop_p))
+
+    def forward(self, g, h):
+        adj_ms = []
+        indices_list = []
+        down_outs = []
+        hs = []
+        org_h = h
+        for i in range(self.l_n):
+            h = self.down_gcns[i](g, h)
+            adj_ms.append(g)
+            down_outs.append(h)
+            g, h, idx = self.pools[i](g, h)
+            indices_list.append(idx)
+        h = self.bottom_gcn(g, h)
+        for i in range(self.l_n):
+            up_idx = self.l_n - i - 1
+            g, idx = adj_ms[up_idx], indices_list[up_idx]
+            g, h = self.unpools[i](g, h, down_outs[up_idx], idx)
+            h = self.up_gcns[i](g, h)
+            h = h.add(down_outs[up_idx])
+            hs.append(h)
+        h = h.add(org_h)
+        hs.append(h)
+        return hs
+
+
+class GCN(nn.Module):
+
+    def __init__(self, in_dim, out_dim, act, p):
+        super(GCN, self).__init__()
+        self.proj = nn.Linear(in_dim, out_dim)
+        self.act = act
+        self.drop = nn.Dropout(p=p) if p > 0.0 else nn.Identity()
+
+    def forward(self, g, h):
+        h = self.drop(h)
+        h = torch.matmul(g, h)
+        h = self.proj(h)
+        h = self.act(h)
+        return h
+
+
+class Pool(nn.Module):
+
+    def __init__(self, k, in_dim, p):
+        super(Pool, self).__init__()
+        self.k = k
+        self.sigmoid = nn.Sigmoid()
+        self.proj = nn.Linear(in_dim, 1)
+        self.drop = nn.Dropout(p=p) if p > 0 else nn.Identity()
+
+    def forward(self, g, h):
+        Z = self.drop(h)
+        # find jaccard coefficients for every node in z
+        zlist = Z.tolist()
+        num_nodes = g.shape[0]
+        # store jaccard coefficient for every u,v pair
+        jaccscores = [0] * num_nodes
+
+       #print("finding jaccard coefficients")
+        jacc = [0] * num_nodes
+        for j in range(len(jacc)):
+            jacc[j] = [0] * num_nodes
+        for u in range(len(zlist)):
+            for v in range(len(zlist)):
+                if u != v:
+                    jacc[u][v] = calcJaccard(Z,u,v)
+
+        #print("ranking")
+        nodes = []
+        for k in range(int(self.k * num_nodes)):
+            # find largest value in jacc
+            maxjacc = float('-inf')
+            uind = -1
+            for u in range(len(jacc)):
+                if u in nodes: continue
+                if max(jacc[u]) > maxjacc:
+                    maxjacc = max(jacc[u])
+                    uind = u
+            nodes.append(uind)
+        for n in nodes:
+            jaccscores[n] = 1
+        jaccscores = torch.FloatTensor(jaccscores)
+        return top_k_graph(jaccscores, g, h, self.k)
+
+
+class Unpool(nn.Module):
+
+    def __init__(self, *args):
+        super(Unpool, self).__init__()
+
+    def forward(self, g, h, pre_h, idx):
+        new_h = h.new_zeros([g.shape[0], h.shape[1]])
+        new_h[idx] = h
+        return g, new_h
+
+# to find # of neighbors, take # of non-zero elements for corresponding row in adjacency matrix
+def calcJaccard(g,idxu,idxv):
+    nu,nv = g[idxu].tolist(), g[idxv].tolist()
+    if max(nu) == 0 and max(nv) == 0:
+        return 0
+    intr, uni = 0, 0
+    for i in range(len(nu)):
+        # if neighbor is shared
+        if nu[i] != 0 and nv[i] != 0:
+            intr = intr + 1
+        # if at least one of the nodes has a neighbor
+        if nu[i] != 0 or nv[i] != 0:
+            uni = uni + 1
+    return intr/uni
+
+# to change
+def top_k_graph(scores, g, h, k):
+    num_nodes = g.shape[0]
+    values, idx = torch.topk(scores, max(2, int(k*num_nodes)))
+    new_h = h[idx, :]
+    values = torch.unsqueeze(values, -1)
+    new_h = torch.mul(new_h, values)
+    un_g = g.bool().float()
+    un_g = torch.matmul(un_g, un_g).bool().float()
+    un_g = un_g[idx, :]
+    un_g = un_g[:, idx]
+    g = norm_g(un_g)
+    return g, new_h, idx
+
+
+def norm_g(g):
+    degrees = torch.sum(g, 1)
+    g = g / degrees
+    return g
+
+
+class Initializer(object):
+
+    @classmethod
+    def _glorot_uniform(cls, w):
+        if len(w.size()) == 2:
+            fan_in, fan_out = w.size()
+        elif len(w.size()) == 3:
+            fan_in = w.size()[1] * w.size()[2]
+            fan_out = w.size()[0] * w.size()[2]
+        else:
+            fan_in = np.prod(w.size())
+            fan_out = np.prod(w.size())
+        limit = np.sqrt(6.0 / (fan_in + fan_out))
+        w.uniform_(-limit, limit)
+
+    @classmethod
+    def _param_init(cls, m):
+        if isinstance(m, nn.parameter.Parameter):
+            cls._glorot_uniform(m.data)
+        elif isinstance(m, nn.Linear):
+            m.bias.data.zero_()
+            cls._glorot_uniform(m.weight.data)
+
+    @classmethod
+    def weights_init(cls, m):
+        for p in m.modules():
+            if isinstance(p, nn.ParameterList):
+                for pp in p:
+                    cls._param_init(pp)
+            else:
+                cls._param_init(p)
+
+        for name, p in m.named_parameters():
+            if '.' not in name:
+                cls._param_init(p)
